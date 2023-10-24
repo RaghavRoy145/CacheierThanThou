5aa18831167dba72fa5a934c4b79548f14498df8
Raghav Roy <raghavroy145@gmail.com>
2023-10-24 10:55:20 +0530

    hacking with hashicorp raft

--- client/runtest/main.go
+++ client/runtest/main.go
@@ -4,13 +4,48 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
+	"os"
 	"time"
 
+	"github.com/hashicorp/raft"
 	"github.com/raghavroy145/DistributedCaching/client"
 )
 
+type Server struct {
+	raft *raft.Raft
+}
+
 func main() {
-	SendStuff()
+	var (
+		cfg           = raft.DefaultConfig()
+		fsm           = &raft.MockFSM{}
+		logStore      = raft.NewInmemStore()
+		stableStore   = raft.NewInmemStore()
+		snapShotStore = raft.NewInmemSnapshotStore()
+		timeout       = time.Second * 5
+		// logOutout     = new(bytes.Buffer)
+	)
+	cfg.LocalID = "royboi"
+	// copied from hashicorp raft test code
+	ips, err := net.LookupIP("localhost")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(ips) == 0 {
+		log.Fatal("localhost did not resolve to any IPs")
+	}
+	addr := &net.TCPAddr{IP: ips[0], Port: 4000}
+	tr, err := raft.NewTCPTransport(":4000", addr, 10, timeout, os.Stdout)
+	if err != nil {
+		log.Fatal("TCP net failed", err)
+	}
+	r, err := raft.NewRaft(cfg, fsm, stableStore, logStore, snapShotStore, tr)
+	if err != nil {
+		log.Fatal("Failed to create new raft: ", err)
+	}
+	fmt.Printf("%+v\n", r)
+	select {}
 }
 
 func SendStuff() {
--- server.go
+++ server.go
@@ -12,6 +12,7 @@ import (
 	"github.com/raghavroy145/DistributedCaching/cache"
 	"github.com/raghavroy145/DistributedCaching/client"
 	"github.com/raghavroy145/DistributedCaching/proto"
+	"go.uber.org/zap"
 )
 
 type ServerOpts struct {
@@ -23,13 +24,18 @@ type Server struct {
 	ServerOpts
 	members map[client.Client]struct{} //Why map? Because O(1)
 	cache   cache.Cacher
+	logger  *zap.SugaredLogger
 }
 
 func NewServer(opts ServerOpts, c cache.Cacher) *Server {
+	l, _ := zap.NewProduction()
+	lsugar := l.Sugar()
+	l.With()
 	return &Server{
 		ServerOpts: opts,
 		cache:      c,
 		members:    make(map[client.Client]struct{}),
+		logger:     lsugar,
 	}
 }
 
@@ -46,7 +52,7 @@ func (s *Server) Start() error {
 			}
 		}()
 	}
-	log.Printf("server on starting on port [%s]\n", s.ListenAddr)
+	s.logger.Infow("server starting", "addr", s.ListenAddr, "leader", s.IsLeader)
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
@@ -76,7 +82,6 @@ func (s *Server) handleConn(conn net.Conn) {
 }
 
 func (s *Server) handleCommand(conn net.Conn, cmd any) {
-
 	switch v := cmd.(type) {
 	case *proto.CommandSet:
 		s.handleSetCommand(conn, v)
@@ -138,7 +143,7 @@ func (s *Server) dialLeader() error {
 		return fmt.Errorf("failed to dial leader: [%s]", s.LeaderAddr)
 	}
 
-	log.Println("connected to leader:", s.LeaderAddr)
+	s.logger.Infow("connected to leader", "addr", s.LeaderAddr)
 	binary.Write(conn, binary.LittleEndian, proto.CmdJoin)
 	s.handleConn(conn)
 	return nil
